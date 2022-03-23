c821eb983deb30c2e894d4e089620c812acdeb13
A. RAHMAN <96154091+rahmanumar21@users.noreply.github.com>
2022-03-23 21:51:42 +0100

    update 220
    update220

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,5 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const SecurePasssssssdfdfdfge = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
